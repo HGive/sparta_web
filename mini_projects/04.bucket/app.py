e288dc0e62aa986ab224dc9d11dc082f5431da72
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-06 13:36:07 +0900

    work

--- mini_projects/04.bucket/app.py
+++ mini_projects/04.bucket/app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('mongodb+srv://sparta:test@cluster0.x6y4kbq.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
-
 
 @app.route('/')
 def home():
@@ -13,15 +13,29 @@ def home():
 @app.route("/bucket", methods=["POST"])
 def bucket_post():
     bucket_receive = request.form['bucket_give']
-    doc= {
-        'bucket':bucket_receive
+
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    count = len(bucket_list) + 1
+    doc = {
+        'num':count,
+        'bucket' : bucket_receive,
+        'done' : 0
     }
     db.bucket.insert_one(doc)
-    return jsonify({'msg': '저장완료!'})
+    return jsonify({'msg': '저장 완료!'})
+
+@app.route("/bucket/done", methods=["POST"])
+def done_post():
+    number_receive = request.form['num_give']
+
+    db.bucket.update_one({'num':int(number_receive)},{'$set':{'done':1}})
+    
+    return jsonify({'msg': '버킷 완료!'})
+
     
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
-    all_buckets = list(db.bucket.find({},{'_id':False}))
+    all_buckets = list(db.bucket.find({}, {'_id': False}))
     return jsonify({'result': all_buckets})
 
 if __name__ == '__main__':
